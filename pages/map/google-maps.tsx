fd82bdb5226904b3ecf035d11fe878b595e7cec8
jacksonmccluskey <jacksonmccluskey@protonmail.com>
2024-09-25 14:39:53 -0700

    Just Google Maps

--- pages/map/google-maps.tsx
+++ pages/map/google-maps.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 import { config } from '../../config';
-import PoiMarkers from './poi-markers';
 
 const mapContainerStyle = { width: '100vw', height: '100vh' };
 const center = { lat: 32, lng: -70 };
-
-const locations = [
-	{ lat: 32.1, lng: -117.1 },
-	{ lat: 32.2, lng: -117.2 },
-	{ lat: 32.4, lng: -117.3 },
-	{ lat: 32.6, lng: -117.4 },
-	{ lat: 32.8, lng: -117.4 },
-];
 
 export const GoogleMaps = () => {
 	const { isLoaded, loadError } = useLoadScript({
@@ -31,9 +22,7 @@ export const GoogleMaps = () => {
 				gestureHandling: 'greedy',
 				disableDefaultUI: true,
 			}}
-		>
-			<PoiMarkers locations={locations} />
-		</GoogleMap>
+		/>
 	);
 };
 
